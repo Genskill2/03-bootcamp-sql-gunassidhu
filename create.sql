721814e79878204781ab2c1f46480f82b848d17d
Gunasagar Pullamchetty <gsagarsidhu@gmail.com>
2021-06-19 12:03:18 +0530

    Added the SQL files

---
+++ create.sql
@@ -0,0 +1,32 @@
+--publishertable
+create table publisher(
+    id integer primary key,
+    name text,
+    country text
+);
+
+--bookstable
+create table books(
+    id integer primary key,
+    title text,
+    publisher integer, 
+    foreign key (publisher)
+      references publisher(id)
+);
+
+--subjectstable
+create table subjects(
+    id integer primary key,
+    name text
+);
+
+--books_subjectstable
+create table books_subjects(
+   book integer,
+   subject integer, 
+   foreign key (book)
+      references books(id)
+ 
+   foreign key (subject)
+	references subjects(id)
+);
